8670df8c4f9a85d63a6b3856590319cbde80fb69
Пользователь <kuleshov000@gmail.com>
2023-10-16 13:38:53 +0300

    add: docs page

--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -7,3 +7,13 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+// Слайдер "УСТАВНЫЕ ДОКУМЕНТЫ КОМПАНИИ"
+const swiperDocs = new Swiper('.legally__swiper', {
+    spaceBetween: 24,
+    slidesPerView: 4,
+    grabCursor: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
