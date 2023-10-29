f418dba337861975de6772243fd23ba34a31d0aa
Пользователь <kuleshov000@gmail.com>
2023-10-29 18:31:19 +0300

    add hideOrShowButtons.js

---
+++ dist/assets/js/hideOrShowButtons.js
@@ -0,0 +1,34 @@
+const navigation = document.querySelector(".navigation");
+const cards = document.querySelector(".cards");
+const headerButtons = document.querySelector(".header__buttons");
+const headerWrapper = document.querySelector(".header__wrapper");
+
+let lastScroll = 0;
+const defaultOffset = 100;
+
+const scrollPosition = () => document.documentElement.scrollTop;
+
+const containHide = () => {
+    if (headerButtons.classList.contains('hidden') &&
+        headerWrapper.classList.contains('hidden') &&
+        navigation.classList.contains('hidden')) {
+        return true;
+    }
+}
+
+window.addEventListener('scroll', () => {
+    if (scrollPosition() > lastScroll && !containHide() && scrollPosition() && scrollPosition() > defaultOffset) {
+        //scroll down
+        headerButtons.classList.add('hidden');
+        headerWrapper.classList.add('hidden');
+        navigation.classList.add('hidden');
+    }
+    else if (scrollPosition() < lastScroll && containHide()) {
+        //scroll up
+        headerButtons.classList.remove('hidden');
+        headerWrapper.classList.remove('hidden');
+        navigation.classList.remove('hidden');
+    }
+
+    lastScroll = scrollPosition();
+})
