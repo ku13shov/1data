022951ecb39bf0c7fa7ac1ff44074982e8342f12
Пользователь <kuleshov000@gmail.com>
2023-11-01 00:10:11 +0300

    adaptive cards

--- app/assets/js/swiper.js
+++ app/assets/js/swiper.js
@@ -34,22 +34,45 @@ const swiperDocs = new Swiper('.legally__swiper', {
 });
 
 // Слайдер в каталоге
-const swiperCatalog = new Swiper('.catalog__swiper', {
-    spaceBetween: 30,
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    grabCursor: true,
-    keyboard: {
-        enabled: true,
-    },
+if (window.innerWidth > 550) {
+    const swiperCatalog = new Swiper('.catalog__swiper', {
+        spaceBetween: 30,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        grabCursor: true,
+        keyboard: {
+            enabled: true,
+        },
 
-    navigation: {
-        nextEl: '.catalog__swiper-button-next',
-        prevEl: '.catalog__swiper-button-prev',
-    },
+        navigation: {
+            nextEl: '.catalog__swiper-button-next',
+            prevEl: '.catalog__swiper-button-prev',
+        },
 
-    pagination: {
-        el: ".catalog__swiper-pagination",
-        clickable: true,
-    },
-});
+        pagination: {
+            el: ".catalog__swiper-pagination",
+            clickable: true,
+        },
+
+        breakpoints: {
+            // 320: {
+            //     slidesPerView: 2,
+            //     slidesPerGroup: 2,
+            //     direction: "vertical",
+            // },
+            550: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 24,
+            },
+            1279: {
+                slidesPerView: 4,
+            },
+        },
+    });
+}
